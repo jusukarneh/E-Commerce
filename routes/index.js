8f35f0db164a74153d2c470fa61c8c6d3fcf6c2d
jusukarneh <jusudaddi@yahoo.com>
2023-12-12 20:12:01 -0500

    delete comment and added routes in the const

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
+const apiRoutes = require('../routes/api');
 
-//http://localhost:3001/api
+
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>")
+
 });
 
 module.exports = router;
